4ac92fade0373b9c27964355b4fe59f76e611d7f
emerald32-source <emerald320032@gmail.com>
2026-05-29 09:06:56 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+const clientCustomName = "داريــن";
+const clientGender = "f";
+const clientDate = "2026-06-14";
+const clientEnvelopeText = `كل سنة و انتي طيبة يا دارين عقبال 100 سنه🤍
+اتمنى تكوني مبسوطه ف حياتك!
+كان نفسي اعملك حاجه اكبر من كدا بس الموضوع صعب شويه اعذريني🤏🏻
+اتبسطي بكل حاجه في حياتك و متخليش اي حاجه تزعلك و تفكري كتير في حاجات ملهاش لازمه تصدعك اخر الليل
+جبتلك شويه ورد عشان عارف انك بتحبي الورد حافظي عليه بقا !!
+ياريت تكون الهديه عجبتك🤍
+يا اجمل عيون شافتها عيني
+يا احلى واحده شفتها في حياتي
+كل الحب🫂🤍`;
+const clientMemories = [
+    {
+        "photo": "photo1.jpg",
+        "caption": "الخير و السعاده"
+    },
+    {
+        "photo": "photo2.jpg",
+        "caption": "دايما"
+    },
+    {
+        "photo": "photo3.jpg",
+        "caption": "و هتمنالك"
+    },
+    {
+        "photo": "photo4.jpg",
+        "caption": "حياتي كلها"
+    },
+    {
+        "photo": "photo5.jpg",
+        "caption": "حصلت في"
+    },
+    {
+        "photo": "photo6.jpg",
+        "caption": "أحسن حاجه"
+    },
+    {
+        "photo": "photo7.jpg",
+        "caption": "هتفضلي أنتي"
+    },
+    {
+        "photo": "photo8.jpg",
+        "caption": "عدت السنين"
+    },
+    {
+        "photo": "photo9.jpg",
+        "caption": "حتى لو"
+    }
+];
